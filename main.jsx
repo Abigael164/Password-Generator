f38d950c455c2cf47e7ff33afa1a3bd323711a8b
Abigael Jeptoo <jeptooabigael164@gmail.com>
2025-11-14 12:50:33 -0800

    Render the app

---
+++ main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+//import Clock from './Clock.jsx'
+//import App from './App.jsx'
+//import Calculate from './Calculate.jsx'
+//import Myprop from './Myprop.jsx'
+//import Reactlist from './Reactlist.jsx'
+import Password from './Password.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Password />
+  </StrictMode>,
+);
